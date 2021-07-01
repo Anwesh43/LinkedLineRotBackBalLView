9a8afbf5674b94e3f5f0f4ac64b99df662e0d2b3
Anwesh43 <anweshthecool0@gmail.com>
2021-07-02 01:24:19 +0530

    adding constants and libraries

--- linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
+++ linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
@@ -1,2 +1,27 @@
 package com.example.linerotbackview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#B71C1C",
+    "#304FFE",
+    "#E65100",
+    "#AA00FF",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.4f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 120f
+
