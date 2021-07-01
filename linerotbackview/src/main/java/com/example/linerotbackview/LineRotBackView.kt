8c9b00dc438ef77c97f8c898941f7ed9c58b053d
Anwesh43 <anweshthecool0@gmail.com>
2021-07-02 01:32:05 +0530

    adding Canvas extension methods

--- linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
+++ linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
@@ -23,8 +23,31 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.4f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
-val rot : Float = 120f
+val rot : Float = 150f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawLineRotBack(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val r : Float = size / 5
+    save()
+    translate(w / 2, 0f)
+    rotate(rot * sc3)
+    drawLine(0f, 0f, size * sc1, 0f, paint)
+    drawCircle(0f, -h / 2 - r + (h / 2 + r) * sc2, r, paint)
+    restore()
+}
+
+fun Canvas.drawLRBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotBack(scale, w, h, paint)
+}
