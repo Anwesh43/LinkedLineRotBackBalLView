099f83807a63b30a67390f0689183264298913a0
Anwesh43 <anweshthecool0@gmail.com>
2021-07-02 01:34:26 +0530

    adding LineRotBackView class

--- linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
+++ linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
@@ -51,3 +51,19 @@ fun Canvas.drawLRBNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineRotBack(scale, w, h, paint)
 }
+
+class LineRotBackView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+ }
