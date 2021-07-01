356ba117fbafa2a0dad4ecf33a1587575d86270f
Anwesh43 <anweshthecool0@gmail.com>
2021-07-02 01:58:26 +0530

    adding LineRotBreak class

--- linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
+++ linerotbackview/src/main/java/com/example/linerotbackview/LineRotBackView.kt
@@ -155,4 +155,27 @@ class LineRotBackView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineRotBack(var i : Int) {
+
+        private var curr : LRBNode = LRBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
  }
